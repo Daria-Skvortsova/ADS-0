f4acf0e3ff5e12ff68535efe94d059819a2b72d3
Daria-Skvortsova <99176875+Daria-Skvortsova@users.noreply.github.com>
2022-02-13 15:35:28 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,11 @@
 // Copyright 2021 NNTU-CS
 int gcd(int a, int b) {
-	if (b == 0) {
-		return a;
-	}
-	else {
-		return gcd(b, a % b);
-	}
-	
+    while (a != b) {
+        if (a > b) {
+            a -= b;
+        } else {
+            b -= a;
+        }
+    }
+    return a;
 }
